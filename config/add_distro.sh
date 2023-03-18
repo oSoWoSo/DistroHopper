a752460eab3afc08c1f4069f754a8db3e212a76d
zenobit <zen@osowoso.xyz>
2023-03-18 14:54:46 +0100

    Use tmp dir

--- config/add_distro.sh
+++ config/add_distro.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
+TMP_DIR="/tmp"
+yad --form --field="Pretty name" "" --field="Name" "" --field="Releases" "" --field="Editions" "" --field="URL" "" --field="ISO" "" --field="Hash" "" > ${TMP_DIR}/template.tmp
 
-yad --form --field="Pretty name" "" --field="Name" "" --field="Releases" "" --field="Editions" "" --field="URL" "" --field="ISO" "" --field="Hash" "" > template.tmp
 
-
-PRETTY_NAME="$(cat template.tmp | cut -d'|' -f1)"
-NAME="$(cat template.tmp | cut -d'|' -f2)"
-RELEASES="$(cat template.tmp | cut -d'|' -f3)"
-EDITIONS="$(cat template.tmp | cut -d'|' -f4)"
-URL="$(cat template.tmp | cut -d'|' -f5)"
-ISO="$(cat template.tmp | cut -d'|' -f6)"
-HASH="$(cat template.tmp | cut -d'|' -f7)"
+PRETTY_NAME="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f1)"
+NAME="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f2)"
+RELEASES="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f3)"
+EDITIONS="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f4)"
+URL="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f5)"
+ISO="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f6)"
+HASH="$(cat ${TMP_DIR}/template.tmp | cut -d'|' -f7)"
 echo "    $NAME)           PRETTY_NAME=$PRETTY_NAME;;
-" > newvm.tmp
+" >  ${TMP_DIR}/newvm.tmp
 echo "    $NAME \\
-" >> newvm.tmp
+" >>  ${TMP_DIR}/newvm.tmp
 echo "function releases_$NAME() {
     echo $RELEASES
 }
-" >> newvm.tmp
+" >>  ${TMP_DIR}/newvm.tmp
 echo "function editions_$NAME() {
     echo $EDITIONS
 }
-" >> newvm.tmp
+" >>  ${TMP_DIR}/newvm.tmp
 echo "function get_$NAME() {
     local EDITION="${1:-}"
     local HASH=""
@@ -30,10 +30,10 @@ echo "function get_$NAME() {
     HASH=\"$(wget -q -O- "${URL}/CHECKSUM" | grep "(${ISO}" | cut -d' ' -f4)\"
     echo "${URL}/${ISO} ${HASH}"
 }
-" >> newvm.tmp
+" >>  ${TMP_DIR}/newvm.tmp
 echo "template.tmp content:
 "
-cat template.tmp
+cat  ${TMP_DIR}/template.tmp
 echo "newvm.tmp content:
 "
-cat newvm.tmp
+cat  ${TMP_DIR}/newvm.tmp
